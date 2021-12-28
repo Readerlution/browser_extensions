5f349e3d27205f8a9db1c9a48752e51d68202054
Readerlution <jhamiltona@gmail.com>
2021-12-28 19:14:38 +0900

    video speed manipulator extension

---
+++ speed.js
@@ -0,0 +1,10 @@
+( () => {   
+        window.addEventListener("keypress", function(e) {
+            if (e.key=== "+") {
+                document.querySelector('video').playbackRate = 20 ;
+            } else if (e.key=== "-") {
+                document.querySelector('video').playbackRate = 1 ;
+            }
+        }
+    )}
+)();
