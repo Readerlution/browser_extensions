b717ecd4c4b0b700f5366bdd294622ea446a8da2
Readerlution <jhamiltona@gmail.com>
2021-12-30 04:16:51 -0500

    located classes and commands to close ads

--- overlay_ads.js
+++ overlay_ads.js
@@ -1,3 +1,7 @@
 // youtube video screen
-document.getElementById("movie_player");
-document.getElementsByClassName("ytp-ad-skip-button")
+var skip_button = document.getElementsByClassName("ytp-ad-skip-button"); // grab skip_button during ads
+skip_button[0].click(); // skip the ad instantly
+
+// small ads that are overlayed on top of youtube screens
+var small_ad = document.getElementsByClassName("ytp-ad-overlay-image"); // grab ad element
+small_ad[0].parentNode.removeChild(small_ad[0]); // delete ad element
