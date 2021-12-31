6bafe6df2fc35b512511b73b55e9a1d715a338d1
Readerlution <jhamiltona@gmail.com>
2021-12-31 08:58:32 -0500

    yt ad skipper functions

--- overlay_ads.js
+++ overlay_ads.js
@@ -1,7 +1,28 @@
-// youtube video screen
-var skip_button = document.getElementsByClassName("ytp-ad-skip-button"); // grab skip_button during ads
-skip_button[0].click(); // skip the ad instantly
+var is_youtube = document.location.host === "www.youtube.com";
 
-// small ads that are overlayed on top of youtube screens
-var small_ad = document.getElementsByClassName("ytp-ad-overlay-image"); // grab ad element
-small_ad[0].parentNode.removeChild(small_ad[0]); // delete ad element
+if (is_youtube) {
+
+		// full screen ads with skip button
+		var check_skipbtn_exist = setInterval(function() {
+			var skip_button = document.getElementsByClassName("ytp-ad-skip-button"); // grab skip_button during ads
+			if ( skip_button.length)  {
+				skip_button[0].click(); // skip the ad instantly
+			}
+		}, 100);
+
+		// small ads that are overlayed on top of youtube screens
+		var check_smallAd_exist = setInterval(function() {
+			var small_ad = document.getElementsByClassName("ytp-ad-overlay-image"); // grab ad element
+			if ( small_ad.length ) {
+				small_ad[0].parentNode.removeChild(small_ad[0]); // delete ad element
+			}
+		}, 100);
+
+		// text ads overlayed on top of screen
+		var check_textAd_exist = setInterval(function() {
+			var textAd = document.getElementsByClassName("ytp-ad-overlay-slot"); // grab ad element
+			if ( textAd.length ) {
+				textAd[0].parentNode.removeChild(textAd[0]); // delete ad element
+			}
+		}, 100);
+}
--- speed/speed.js
+++ speed/speed.js
@@ -1,10 +1,9 @@
-( () => {   
-        window.addEventListener("keypress", function(e) {
-            if (e.key=== "+") {
-                document.querySelector('video').playbackRate = 20 ;
-            } else if (e.key=== "-") {
-                document.querySelector('video').playbackRate = 1 ;
-            }
+(() => {
+    window.addEventListener("keypress", function (e) {
+        if (e.key === "+") {
+            document.querySelector('video').playbackRate = 20;
+        } else if (e.key === "-") {
+            document.querySelector('video').playbackRate = 1;
         }
-    )}
-)();
+    })
+})();
